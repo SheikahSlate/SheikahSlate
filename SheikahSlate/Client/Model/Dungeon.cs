43a968ab3806b60a7fdffea7ab0f5df8ee47ef47
Marc <marcbabtist@gmail.com>
2021-07-22 21:54:10 +0200

    Create start of item state model

---
+++ SheikahSlate/Client/Model/Dungeon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SheikahSlate.Client.Model
+{
+    public class Dungeon
+    {
+        public byte Keys { get; set; }
+        public bool BossKey { get; set; }
+        public bool Map { get; set; }
+        public bool Compass { get; set; }
+        public DungeonBossDrop BossDrop { get; set; }
+        public Version Version { get; set; }
+    }
+}
