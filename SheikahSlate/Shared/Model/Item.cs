f12adb0eec3dc2ad6c4d452a5de572de56914dbe
Dvohoritch <afejja@gmail.com>
2021-07-31 17:59:10 +0200

    Add in/decrement on images

--- SheikahSlate/Shared/Model/Item.cs
+++ SheikahSlate/Shared/Model/Item.cs
@@ -5,13 +5,37 @@ using System.Threading.Tasks;
 
 namespace SheikahSlate.Shared.Model
 {
-    public class Item
+    public partial class Item
     {
         public string ItemName { get; init; } = "Default Item (THIS IS BROKEN)";
         public int ItemId { get; init; } = -1;
-        public int CurrentState { get; set; } = 0;
+        private int currentState = 0;
+        public int CurrentState 
+        { 
+            get 
+            {
+                return currentState;
+            } 
+            set
+            {
+                if (value > MaxState)
+                {
+                    currentState = MaxState;
+                }
+                else if (value < 0)
+                {
+                    currentState = 0;
+                }
+                else
+                {
+                    currentState = value;
+                }
+            }
+        }
         public int MaxState { get; init; } = 1;
         //TODO: Meer?
+
+        public string ImageUrl => $"{ItemId}-{currentState}.jpg";
 
     }
 }
