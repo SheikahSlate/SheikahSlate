496801cd147f13f5f539f1949d73844979505269
Marc <marcbabtist@gmail.com>
2021-08-30 16:10:48 +0200

    Add dungeon listing

---
+++ SheikahSlate/Shared/Model/Dungeon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheikahSlate.Shared.Model
+{
+    public class Dungeon
+    {
+        public string Name { get; init; } = "Default Name";
+        public string Shorthand { get; init; } = "Dflt";
+        public int Id { get; init; } = -1;
+        public bool HasReward { get; init; }
+        public IEnumerable<Item> Items { get; init; } = Enumerable.Empty<Item>();
+    }
+}
